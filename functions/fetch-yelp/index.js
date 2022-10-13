c6d06e4d687ade14ada38d82907a1a3649725971
Tanner Richards <Tanner.richards990@gmail.com>
2022-10-12 17:34:41 -0700

    finished the try catch func in fetch-yelp index.js

--- functions/fetch-yelp/index.js
+++ functions/fetch-yelp/index.js
@@ -2,8 +2,35 @@ const fetch = require('node-fetch');
 require('dotenv').config({ path: `.env.development.local` });
 
 const handler = async (event) => {
-  // add code here to fetch data from yelp API
-  // be sure to include the parameters from event.queryStringParameters
+
+const zip = event.queryStringParameters.zip;
+const search = event.queryStringParameters.search;
+
+try {
+  const response = await fetch(
+    `https://api.yelp.com/v3/businesses/search?term=${search}&location=${zip}`, 
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+      },
+    }
+  );
+
+  const data = await response.json();
+  const json = JSON.stringify(data.businesses);
+
+  return {
+    statusCode: 200,
+    body: json,
+  };
+}
+catch (error) {
+  console.log(error);
+  return {
+    statusCode: 500,
+    body: JSON.stringify({ error: 'Failed to grab the data' }),
+  };
+}
 };
 
 module.exports = { handler };
